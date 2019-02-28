fa5598e6bb5aa0ff872bfc7f9599425dee25e27d
Justine B <justinegayton@gmail.com>
2019-02-28 21:30:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,6 @@ printBadges(names);
 
 
 function tailsNeverFails() {
-var coinFace = Math.floor(Math.random() * 2);
-
-while(coinFace === 0){
-    console.log("Heads! Flipping again...");
-    var coinFace = Math.floor(Math.random() * 2);
-}
-console.log("Tails! Done flipping.");
+Math.random() >= 0.5
 
 }
