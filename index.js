245ed8de8f740e83e227c499591067126e1ebe2c
Justine B <justinegayton@gmail.com>
2019-02-28 20:55:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,13 +5,3 @@ function printBadges(badges) {
   console.log();
   return badges;
 }
-
-function wrapGifts(gifts) {
-    for (let i = 0; i < gifts.length; i++) {
-        console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    }
- 
-    return gifts;
-}
- 
-wrapGifts(gifts);
