600a8182d5c7f351227033c60c6514f0f8569314
Justine B <justinegayton@gmail.com>
2019-02-28 21:29:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ function printBadges(names) {
 printBadges(names);
 
 
-// function tailsNeverFails() {
-// var coinFace = Math.floor(Math.random() * 2);
+function tailsNeverFails() {
+var coinFace = Math.floor(Math.random() * 2);
 
-// while(coinFace === 0){
-//     console.log("Heads! Flipping again...");
-//     var coinFace = Math.floor(Math.random() * 2);
-// }
-// console.log("Tails! Done flipping.");
+while(coinFace === 0){
+    console.log("Heads! Flipping again...");
+    var coinFace = Math.floor(Math.random() * 2);
+}
+console.log("Tails! Done flipping.");
 
-// }
+}
