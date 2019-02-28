9db9ca0b3b1a3409745a08ee17749d6e18f103dc
Justine B <justinegayton@gmail.com>
2019-02-28 20:54:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // Code your solutions in this file
 
-function printBadges() {
-  
+function printBadges(badges) {
+  for let i = 0 
+  console.log();
+  return badges;
 }
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+ 
+    return gifts;
+}
+ 
+wrapGifts(gifts);
