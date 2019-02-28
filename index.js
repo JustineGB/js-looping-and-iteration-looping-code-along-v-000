998638947956ecd8cc3dfd924e45091c9f1844ee
Justine B <justinegayton@gmail.com>
2019-02-28 20:57:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 
 function printBadges(badges) {
   for (let i = 0; i < badges.length; i++) {  
-  console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  console.log(`Welcome ${name[i]} and added a bow!`);
     }
     return gifts;
 }
+'Ada', 'Brendan', 'Ali']);
+// LOG: Welcome Ada! You are employee #1.
+// LOG: Welcome Brendan! You are employee #2.
+// LOG: Welcome Ali! You are employee #3.
+// => ["Ada", "Brendan", "Ali"]
